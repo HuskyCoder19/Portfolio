d9cdbc8b0e00bb89f6dead3b950b8f36c57849da
Jeremiah Kollodge <jtkollodge@gmail.com>
2025-04-23 19:56:56 -0400

    about me and services

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-// about me typing animation
+// header typing animation
 document.addEventListener("DOMContentLoaded", function () {
-    const bio = `I am a professional Software Engineer with a B.S degree in Computer Science from Michigan Technological University. I am driven, hard-working, and a team player dedicated to mission success.
-        As a full stack developer, I have strong experience and expertise in software architecture, object oriented programming, conncurrency, algorithms, and UI/UX design.
-        I take pride in the work I do, and efficiently produce high-quality software that meets user demands and requirements.`;
-    const typingSpeed = 10;
+    const bio = `Jeremiah Kollodge`;
+    const typingSpeed = 50;
     let index = 0;
-    const paragraph = document.getElementById("about-me");
+    const paragraph = document.getElementById("name-header");
 
     function typeText() {
         if (index < bio.length) {
